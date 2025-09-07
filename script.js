9b81c327bab13057f7940c3a440959719e05080c
eggtoasts <129806552+eggtoasts@users.noreply.github.com>
2025-09-07 11:25:55 -0400

    Added choice buttons, reset button, score tracker, and win/lose  UI/Interactivity
    Apply this patch to remove user prompt for R/S/L choices. Instead, everytime one of the R/P/S buttons are picked,
    it will update the respective scores. After a winner is decided, user can choose to reset.

--- script.js
+++ script.js
@@ -4,14 +4,60 @@ const choices = ["rock", "scissors", "paper"];
 let humanScore = 0,
   computerScore = 0;
 
+const choicesRef = document.querySelector(".choices");
+const scoreContainer = document.querySelector(".score");
+
+const humanText = document.querySelector(".human");
+const computerText = document.querySelector(".computer");
+
+let choice;
+
+choicesRef.addEventListener("click", (event) => {
+  if (humanScore < 5 && computerScore < 5) {
+    const humanChoice = getHumanChoice(event);
+    const computerChoice = getComputerChoice();
+
+    playRound(humanChoice, computerChoice);
+
+    if (humanScore == 5 || computerScore == 5) {
+      displayWinOrLose(humanScore, computerScore);
+      //Add a new button asking the user to restart.
+      const restartButton = document.createElement("button");
+      restartButton.textContent = "Play Again?";
+      scoreContainer.appendChild(restartButton);
+
+      restartButton.addEventListener("click", () => {
+        resetGame();
+
+        scoreContainer.removeChild(restartButton);
+      });
+    }
+  }
+});
+
+function displayWinOrLose(humanScore, computerScore) {
+  if (humanScore > computerScore) {
+    alert("u win");
+  } else {
+    alert("u lose!");
+  }
+}
+
+function resetGame() {
+  humanScore = 0;
+  computerScore = 0;
+
+  updateScore();
+}
+
 //Randomly picks between rock, scissors or paper
 function getComputerChoice() {
   return choices[Math.floor(Math.random() * 3)];
 }
 
 //Asks user to pick a choice
-function getHumanChoice() {
-  const choice = prompt("Rock, paper, scissors!");
+function getHumanChoice(event) {
+  choice = event.target.id;
   return choice;
 }
 
@@ -29,26 +75,20 @@ function checkChoice(humanChoice, computerChoice) {
     console.log("You lose!~");
     computerScore++;
   }
+
+  updateScore();
+}
+
+//updates our score UI
+function updateScore() {
+  humanText.textContent = humanScore;
+  computerText.textContent = computerScore;
 }
 
 //Gets choices from user and the computer and passes it into checkChoice
 function playRound(humanChoice, computerChoice) {
-  humanChoice = humanChoice.toLowerCase();
   console.log("Human choice is " + humanChoice);
   console.log("Computer choice is " + computerChoice);
 
   checkChoice(humanChoice, computerChoice);
-
-  console.log(computerChoice);
 }
-
-//Plays the game with 5 rounds
-function playGame() {
-  for (let i = 0; i < 5; i++) {
-    const humanAnswer = getHumanChoice();
-    const computerAnswer = getComputerChoice();
-    playRound(humanAnswer, computerAnswer);
-  }
-}
-
-playGame();
